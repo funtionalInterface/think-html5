c84669f56978d8daed27fd072cda321669be0ba8
vm <vm@qq.com>
2020-06-21 00:33:23 +0800

    final

---
+++ README.md
@@ -0,0 +1,9 @@
+ThinkPHP 6.0
+===============
+* 主要用于页面跳转和提取网页头尾
+* 该网站实现了媒体查询
+* 框架目录结构
+    * think/app/controller/index：控制器，实现页面跳转的方法
+    * think/view/index：视图层，index控制器下的前台展示的html
+    * think/view/layout：存放提取出来网站的头尾
+       
---
+++ app/controller/Index.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace app\controller;
+
+use app\BaseController;
+use think\Session;
+use think\View;
+
+class Index extends BaseController
+{
+    public function login()
+    {
+        return \view();
+    }
+
+    public function index()
+    {
+        \session('name', 'index');
+        return \view('index');
+    }
+
+    public function scenicOverview()
+    {
+        \session('name', 'scenicOverview');
+
+        return \view('scenic_overview');
+    }
+
+    public function courtTravel()
+    {
+        \session('name', 'courtTravel');
+        return \view('court_travel');
+    }
+
+    public function comfortable()
+    {
+        \session('name', 'comfortable');
+        return \view();
+    }
+
+    public function news()
+    {
+        \session('name', 'news');
+        return \view();
+    }
+
+    public function feedback()
+    {
+        \session('name', 'feedback');
+        return \view();
+    }
+
+    public function contact()
+    {
+        \session('name', 'contact');
+        return \view();
+    }
+
+    public function map()
+    {
+        \session('name', 'map');
+        return \view();
+    }
+
+    public function newsShow()
+    {
+        return view('news_show');
+    }
+
+    public function courtTravelShow()
+    {
+        return view('court_travel_show');
+    }
+    public function video(){
+        return \view();
+    }
+}
